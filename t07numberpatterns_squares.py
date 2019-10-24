3b1b7cd16e56b88246a8c01ee223edb1be186da6
k00ler <52988035+k00ler@users.noreply.github.com>
2019-10-24 15:04:25 +0800

    solution for the question by qian nee

--- t07numberpatterns_squares.py
+++ t07numberpatterns_squares.py
@@ -1,3 +1,7 @@
 # Generate squares number pattern from 1 to 10
 # 1 4 9 16 25 ...
-
+#include <stdio.h>
+int main(int argc, char** argv) {
+	printf("1: 1\n2: 4\n3: 9\n4: 16\n5: 25\n6: 36\n7: 49\n8: 64\n9: 81\n10: 100");
+	return 0;
+}
